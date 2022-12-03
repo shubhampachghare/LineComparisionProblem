aec1d6c9c11d6e948638df3a3efb211f0b3ff027
shubhampachghare <shubhampachghare@gmail.com>
2022-12-03 23:33:47 +0530

    [Shubham]- welcome to line comparision

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace LineComparisionProblem
+{
+    public class Line
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation");
+        }
+    }
+}
